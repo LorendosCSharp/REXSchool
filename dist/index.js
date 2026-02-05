6609b9912df1c4b6741a2c0ceac61d52bc53ba24
Leonid Osman <124144453+LorendosCSharp@users.noreply.github.com>
2026-02-05 19:30:56 +0100

    Delete dist directory

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-/*
-REXSchool Copyright (C) 2026 Lorendos aka Leonid
-Full license agrement is under the file LICENSE
-Uses code from AdriDevelopsThings/xschool-node on github
-*/
-export { API as REXSchoolAPI } from './api.js'
-export * from './errors.js'
